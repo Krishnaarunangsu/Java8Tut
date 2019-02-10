8efa2b4435c3b4fcf18a247ffc0e20890cbd4b64
Krishnaarunangsu <arunangsutech@gmail.com>
2019-02-10 18:16:41 +0530

    Added a new Java class for Lambda Implementation

---
+++ src/com/narasimha/tut/Java8LambdaTester3.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.narasimha.tut;
+
+/**
+ * @author arunangsu.sahu
+ *
+ */
+interface FuncInterface {
+  // An abstract function
+  void abstractFun(int x);
+
+  // A non-abstract (or default) function
+  default void normalFun() {
+    System.out.println("Hello");
+  }
+}
+
+
+public class Java8LambdaTester3 {
+
+  /**
+   * 
+   */
+  public Java8LambdaTester3() {
+    // No-arg Constructor
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // lambda expression to implement above
+    // functional interface. This interface
+    // by default implements abstractFun()
+    FuncInterface fobj = (int x) -> System.out.println(2 * x);
+
+    // This calls above lambda expression and prints 10.
+    fobj.abstractFun(5);
+
+  }
+
+}
